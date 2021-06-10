33224b2653d8f20d73fb7ed8c918f07670b215f5
Stack_Pointer <kiranchamp.6457@gmail.com>
2021-06-11 03:43:06 +0530

    changes were added

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const daysE1 = document.getElementById('days');
 const hoursE1 = document.getElementById('hours');
 const mintsE1 = document.getElementById('minutes');
 const secondsE1 = document.getElementById('seconds');
+const name=document.getElementById('name');
 
-
-
+const addname=prompt('Enter the name..!');
 const bdaydate=prompt('Enter you birth-day in 1 Jan 2021 format');
+
 
 
 function count_Timer(){
@@ -18,14 +19,35 @@ function count_Timer(){
     const hours = Math.floor(Total_seconds / 3600) % 24;
     const mints = Math.floor(Total_seconds / 60) % 60;
     const seconds = Math.floor(Total_seconds) % 60;
+    
 
     console.log(days, hours, mints, seconds);
 
     daysE1.innerHTML = days;
-    hoursE1.innerHTML = hours;
-    mintsE1.innerHTML = mints;
-    secondsE1.innerHTML = seconds;
+    hoursE1.innerHTML = formatTime(hours);
+    mintsE1.innerHTML = formatTime(mints);
+    secondsE1.innerHTML = formatTime(seconds);
+    
 
+
+
+if(days==0 & hours==0 & mints==0 & seconds==20)
+{
+    name.innerHTML="Happy Birthday..."+addname;
+}
+else if(days==-1){
+    name.innerHTML="It is  your birthday ..."+addname+" today";
+}
+else if(days<-1 ){
+    name.innerHTML="Your birthday had been finished ..."+addname+ " this year..";
+}
+else{
+    name.innerHTML="Advance Happy Birthday..."+addname;
+}
+
+}
+function formatTime(time){
+    return time < 10 ? (`0${time}`): time;
 }
 
 
